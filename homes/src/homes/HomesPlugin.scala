023caaea5cdb63fe3e7776eac7efcc62ae2f3a3b
Sam Pritchard <ophisbonklol@gmail.com>
2020-07-17 15:34:45 +0100

    Add config fields.

--- homes/src/homes/HomesPlugin.scala
+++ homes/src/homes/HomesPlugin.scala
@@ -1,5 +1,6 @@
 package homes
 
+import java.util.UUID
 import java.util.concurrent.Executors
 
 import cats.effect._
@@ -7,10 +8,27 @@ import doobie._
 import doobie.hikari._
 import org.bukkit.plugin.java.JavaPlugin
 
+import scala.collection.mutable
 import scala.concurrent.ExecutionContext
 
 class HomesPlugin extends JavaPlugin {
+  var cooldownMap: mutable.Map[UUID, Long] = mutable.Map()
+  var cooldownMillis: Option[Long] = _
+  var invulnerabilityMillis: Option[Long] = _
+
+  var maximumHomeCount: Option[Int] = _
+  var allowDeletingHomes: Boolean = _
+  var allowOverridingHomes: Boolean = _
+
+  var allowOverworldHomes: Boolean = _
+  var allowNetherHomes: Boolean = _
+  var allowEndHomes: Boolean = _
+
+  var allowNamingHomes: Boolean = _
+  var homeNameCharLimit: Option[Int] = _
+
   override def onEnable(): Unit = {
+    // Setup database stuff.
     val availableProcessors = math.max(2, sys.runtime.availableProcessors)
     val backingThreadPool = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(availableProcessors))
     implicit val contextShift: ContextShift[IO] = IO.contextShift(backingThreadPool)
